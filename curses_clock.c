e5d2b6b7bc2212f9a01420e22480d7c5f4c7ed9c
Christopher Hicks <chicks.net@gmail.com>
2013-07-20 21:23:40 -0700

    do not require user input, sleep one second

--- curses_clock.c
+++ curses_clock.c
@@ -6,7 +6,8 @@ int main() {
 	initscr();			/* Start curses mode 		  */
 	printw("Hello World !!!");	/* Print Hello World		  */
 	refresh();			/* Print it on to the real screen */
-	getch();			/* Wait for user input */
+//	getch();			/* Wait for user input */
+	sleep(1);
 	endwin();			/* End curses mode		  */
 
 	return 0;
