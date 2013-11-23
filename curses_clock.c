3580c2b220f262473a64ae033494800a4ad3ca7f
Christopher Hicks <chicks.net@gmail.com>
2013-11-23 04:05:36 -0800

    start initializations

--- curses_clock.c
+++ curses_clock.c
@@ -2,12 +2,21 @@
 #include <stdio.h>
 #include <ncurses.h>
 
+void initializations() {
+	cbreak(); // cbreak so they don't have to hit enter to exit
+}
+
 int main() {
-	initscr();			/* Start curses mode 		  */
-	printw("Hello World !!!");	/* Print Hello World		  */
-	refresh();			/* Print it on to the real screen */
-//	getch();			/* Wait for user input */
+	initscr();				/* Start curses mode 		  */
+	printw("Welcome to Curses Clock\n");	/* Print welcome message	  */
+	refresh();				/* Print it on to the real screen */
+
+	initializations();
+	printw("initializations complete");
+	refresh();				/* Print it on to the real screen */
 	sleep(1);
+
+//	getch();			/* Wait for user input */
 	endwin();			/* End curses mode		  */
 
 	return 0;
