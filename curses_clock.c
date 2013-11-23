e1a5a0dcfab16c097ace46c5141eaf5afb3469d0
Christopher Hicks <chicks.net@gmail.com>
2013-11-23 04:23:53 -0800

    loop until key is pressed

--- curses_clock.c
+++ curses_clock.c
@@ -7,6 +7,32 @@ int rows,columns; // window size
 void initializations() {
 	cbreak(); // cbreak so they don't have to hit enter to exit
 	getmaxyx(stdscr, rows, columns); // get current window size
+	nodelay(stdscr, TRUE); // read characters in non-blocking mode
+}
+
+int kbhit () {
+	int ch = getch();
+
+	if (ch != ERR) {
+		ungetch(ch);
+		return(1);
+	} else {
+		return(0);
+	}
+}
+
+void display_clock() {
+	while (1) {
+		clear();
+		mvprintw(10,10,"loop");
+		refresh();	// Print it on to the real screen
+
+		if (kbhit()) {
+			return;
+		}
+
+		sleep(1);
+	}
 }
 
 int main() {
@@ -19,8 +45,9 @@ int main() {
 	printw("(rows=%i columns=%i)\n", rows, columns);
 	refresh();				/* Print it on to the real screen */
 	sleep(1);
+	
+	display_clock(); // show the clock
 
-//	getch();			/* Wait for user input */
 	endwin();			/* End curses mode		  */
 
 	return 0;
